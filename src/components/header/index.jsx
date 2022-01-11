12b620d187dad8f7b8f7fe107bf02d4d22430877
Priscille-LR <76170675+Priscille-LR@users.noreply.github.com>
2022-01-11 11:54:20 +0100

    add logout  functionality

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,8 +1,47 @@
 import logo from '../../assets/argentBankLogo.png';
 import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
 import './header.css';
+import { logInUser } from '../../actions/login';
 
 export function Header() {
+   const dispatch = useDispatch();
+   const isUserLoggedIn = useSelector((state) => state.isUserLoggedIn);
+
+   const handleLogout = () => {
+      dispatch(logInUser(false));
+   };
+
+   let loginLink;
+
+   if (!isUserLoggedIn) {
+      loginLink = (
+         <Link to="/sign-in" className="main-nav-item  main-nav-link ">
+            <i className="fa fa-user-circle sign-in-icon-home"></i>
+            <span>Sign In</span>
+         </Link>
+      );
+   } else {
+      loginLink = (
+         <Link
+            to="/"
+            className="main-nav-item  main-nav-link "
+            onClick={handleLogout}
+         >
+            <div className="user-logout">
+               <div>
+                  <i className="fa fa-user-circle sign-in-icon-home"></i>
+                  <span>Tony</span>
+               </div>
+               <div className="signout">
+                  <i className="fas fa-sign-out-alt sign-in-icon-home"></i>
+                  <span>Sign out</span>
+               </div>
+            </div>
+         </Link>
+      );
+   }
+
    return (
       <nav className="main-nav">
          <Link to="/" className="main-nav-logo  main-nav-link ">
@@ -13,13 +52,7 @@ export function Header() {
             />
          </Link>
          <h1 className="sr-only">Argent Bank</h1>
-
-         <div>
-            <Link to="/sign-in" className="main-nav-item  main-nav-link ">
-               <i className="fa fa-user-circle sign-in-icon-home"></i>
-               <span>Sign In</span>
-            </Link>
-         </div>
+         {loginLink}
       </nav>
    );
 }
